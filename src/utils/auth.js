8ab9565fa526e8d3bb6252c5a6c9db35a360195e
Harry-Praveen <praveenkumar16102001@gmail.com>
2025-03-28 19:07:41 +0530

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -14,3 +14,10 @@ export const logout = () => {
 export const removeToken = () => {
   localStorage.removeItem("token");
 };
+export const setToken = (token) => {
+  localStorage.setItem("token", token);
+};
+
+export const getToken = () => {
+  return localStorage.getItem("token");
+};
